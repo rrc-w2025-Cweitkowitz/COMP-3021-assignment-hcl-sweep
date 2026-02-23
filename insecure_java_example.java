cda097a45656fc9267aacfb53a8001141e6c17f0
Connor Weitkowitz <cweitkowitz@academic.rrc.ca>
2026-02-23 11:46:11 -0600

    Name change somewhere.

--- insecure_java_example.java
+++ insecure_java_example.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class SerializeCookie {
   public static void main(String[] args) {
     Cookie cookieObj = new Cookie();
-    cookieObj.setValue("Fed");
+    cookieObj.setValue("Bob");
     
     try {
       FileOutputStream fos = new FileOutputStream("cookies.ser");
